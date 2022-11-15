781295c77649745383957979615b23807ad07147
Dusan Sarac <duka.sarac@gmail.com>
2022-11-15 17:16:48 +0100

    React grid

---
+++ React-grid/grid-react/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="container">
+      <nav className='navbar'>Navbar</nav>
+      <div className="sidebar">Sidebar</div>
+      <div className="box">Box1</div>
+      
+      <div className="footer">Footer</div>
+
+
+
+    </div>
+  )
+}
+
+export default Header
